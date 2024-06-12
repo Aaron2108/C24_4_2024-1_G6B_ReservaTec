37f78f3fa5957fbb3f6b2ec2769651d1727ebd4e
Oliver Mendez <mendezoliver2108@gmail.com>
2024-06-11 22:39:35 -0500

    creacion de horarios con su tabla horarios terminado

--- src/components/HorarioAdmin/CrearHorario.jsx
+++ src/components/HorarioAdmin/CrearHorario.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import Header from "../NavBar/Header";
 import './horario.css';
 import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
-import { TextField } from '@mui/material';
 import dayjs from 'dayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import axios from 'axios';
 
 const CrearHorario = ({ users, setUsers }) => {
-  const { register, handleSubmit } = useForm();
+  const { control, handleSubmit } = useForm();
+
+  const [dataHorarios, setDataHorarios] = useState([])
+
   const submit = data => {
-    console.log(data);
+    const formatData = {
+      hora_inicio: dayjs(data.hora_inicio).format('HH:mm:ss'),
+      hora_fin: dayjs(data.hora_fin).format('HH:mm:ss')
+    }
+    console.log(formatData);
+    const url = "http://127.0.0.1:8000/api/horarios/"
+    axios.post(url, formatData)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  };
+
+
+  useEffect(() => {
+    const urlHorarios = "http://127.0.0.1:8000/api/horarios/"
+    axios.get(urlHorarios)
+    .then(res => setDataHorarios(res.data))
+    .catch(err => console.log(err))
+  }, [dataHorarios])
+  
+  const handleEdit = (index) => {
+    console.log("Editar horario en posición:", index);
+  };
+
+  const handleDelete = (index) => {
+    console.log("Eliminar horario en posición:", index);
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className="div_inicio_Admin">
         <Header users={users} setUsers={setUsers} />
         <section className="section_crear_horario">
@@ -27,25 +51,88 @@ const CrearHorario = ({ users, setUsers }) => {
         <section>
           <article>
             <form onSubmit={handleSubmit(submit)}>
-            <h1>Complete el Horario</h1>
+            <div className="container mt-4">
+              <div className="row justify-content-center">
+                <div className="col-md-6">
+                  <h1 className="text-center mb-4">Complete el Horario</h1>
+                </div>
+              </div>
+            </div>
               <section className='section_crear_hora'>
-                <MobileTimePicker
+                <Controller
+                  name='hora_inicio'
+                  control={control}
+                  defaultValue={dayjs("2022-04-17T15:30")}
+                  render={({field}) => (
+                    <MobileTimePicker
                   label="Inicio"
-                  defaultValue={dayjs('2022-04-17T15:30')}
-                  renderInput={(params) => <TextField {...params} />}
+                  value={field.value}
+                  onChange={field.onChange}
+                  slotProps={{
+                    textField: {
+                      fullWidth: false,
+                    },
+                  }}
+              />
+                  )}
                 />
-                <MobileTimePicker
-                  label="Fin"
-                  defaultValue={dayjs('2022-04-17T15:30')}
-                  renderInput={(params) => <TextField {...params} />}
+                <Controller
+                  name='hora_fin'
+                  control={control}
+                  defaultValue={dayjs("2022-04-17T15:30")}
+                  render={({field}) => (
+                    <MobileTimePicker
+                  label="Inicio"
+                  value={field.value}
+                  onChange={field.onChange}
+                  slotProps={{
+                    textField: {
+                      fullWidth: false,
+                    },
+                  }}
+              />
+                  )}
                 />
                 <button className='buton_crearHorario' type="submit">Enviar</button>
               </section>
             </form>
           </article>
         </section>
+
+        <article className="d-flex justify-content-center mt-4">
+  <div className="container">
+    <div className="table-responsive">
+      <table className="table table-bordered table-hover table-striped table-light">
+        <thead>
+          <tr className="bg-primary text-white">
+            <th className="align-middle text-center">Hora de Inicio</th>
+            <th className="align-middle text-center">Hora final</th>
+            <th className="align-middle text-center">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataHorarios.map((item, index) => (
+            <tr key={index}>
+              <td className="align-middle text-center">{item.hora_inicio}</td>
+              <td className="align-middle text-center">{item.hora_fin}</td>
+              <td className="align-middle text-center w-auto">
+                <button className="btn btn-secondary me-2" onClick={() => handleEdit(index)}>Editar</button>
+                <button className="btn btn-danger" onClick={() => handleDelete(index)}>Eliminar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+</article>
+
+
+
+
+
+
       </div>
-    </LocalizationProvider>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './components/app/store.js'
 import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <LocalizationProvider>
+     <LocalizationProvider dateAdapter={AdapterDayjs}>
 
     {/* <Provider store={store}> */}
     <App />
